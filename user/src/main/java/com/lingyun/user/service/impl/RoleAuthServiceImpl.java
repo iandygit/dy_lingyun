b2cbb9dc25c73b3692439f618c88ecacac1e39be
zhangbo <329989580@qq.com>
2020-10-28 14:20:35 +0800

    添加事物

--- user/src/main/java/com/lingyun/user/service/impl/RoleAuthServiceImpl.java
+++ user/src/main/java/com/lingyun/user/service/impl/RoleAuthServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -76,6 +77,7 @@ public class RoleAuthServiceImpl implements RoleAuthService {
     }
 
     @Override
+    @Transactional
     public int deleteByRoleIdAndMenuId(Long roleId, Long menuId) {
 
          try {
