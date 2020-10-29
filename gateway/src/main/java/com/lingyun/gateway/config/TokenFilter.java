038f0085bf0936705fc1463d2f34500e74fa55d7
zhangbo <329989580@qq.com>
2020-10-29 14:32:58 +0800

    Content-Type:application/json;charset=UTF-8

--- gateway/src/main/java/com/lingyun/gateway/config/TokenFilter.java
+++ gateway/src/main/java/com/lingyun/gateway/config/TokenFilter.java
@@ -43,6 +43,7 @@ public class TokenFilter implements GlobalFilter, Ordered {
         ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
         response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+
         // 获取当前请求路径
         String url = request.getURI().getPath();
         //从请求头中取得token
