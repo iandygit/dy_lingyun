4ebc6305b7fca9d11f35a2ef58eea0740a329bd5
zhangbo <329989580@qq.com>
2020-11-02 09:38:02 +0800

    额增加

--- start.sh
+++ start.sh
@@ -31,71 +31,28 @@ ps -ef | grep user-auth | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep business-pound | grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | grep gateway | grep -v grep | awk '{print $2}' | xargs kill -9
 
-
-#启动注册中心
-count= ps -ef | grep registry | grep -v "grep" | wc -l
-
 sec=7
-for var in 1 2 3
-do
- if [[ 0 -eq $count]] || [[ -z $count ]]
- then
- #启动注册中心服务
-    nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/registry-1.0-SNAPSHOT.jar  >>${base_dir}/registry.log 2>&1 &
-    echo "break"
-          break
- else
-    echo 正在启动注册中心，等待中....
-    echo  排队 $sec 秒 第 $var 次尝试,
-          sleep $sec
- fi
-done
+#启动注册中心服务
 
-count2= ps -ef | grep registry | grep -v "grep" | wc -l
+nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/registry-1.0-SNAPSHOT.jar  >>${base_dir}/registry.log 2>&1 &
 
-if  [[0 -eq count2]]  || [[ -z $count2 ]]
-then
-   echo 注册中心启动失败....，部署中断
-   exit 1
-fi
+sleep $sec
 
+nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/configserver-SNAPSHOT.jar  >>${base_dir}/configserver.log 2>&1 &
 
-count1= ps -ef | grep connfigserver | grep -v "grep" | wc -l
+sleep $sec
 
-for var in 1 2 3
-  do
-  if  [[0 -eq count1]]  || [[ -z $count1 ]]
-    then
-       echo "开始启动配置服务"
-       nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/configserver-SNAPSHOT.jar  >>${base_dir}/configserver.log 2>&1 &
-       return
-     else
-       echo 正在启配置服务，再次尝试....
-          echo  排队 $sec 秒 , 配置服务第 $var 次尝试
-                sleep $sec
-          echo  "启动失败"
-  fi
-done
+#启动用户服务
+nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/user-1.0-SNAPSHOT.jar  >>${base_dir}/user.log 2>&1 &
+sleep $sec
 
-for var in 1 2 3
-do
- if  [[0 -eq $count1]] || [[ -z $count1 ]]
- then
-    echo 正在启动各项服务，等待中....
-    #启动用户服务
-    nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/user-1.0-SNAPSHOT.jar  >>${base_dir}/user.log 2>&1 &
-    #启动授权服务
-    nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/user-auth-1.0-SNAPSHOT.jar  >>${base_dir}/user-auth.log 2>&1 &
-    #启动磅单服务
-    nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/business-pound-1.0-SNAPSHOT.jar  >>${base_dir}/business-pound.log 2>&1 &
-    #启动网关服务
-    nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/gateway-1.0-SNAPSHOT.jar  >>${base_dir}/gateway.log 2>&1 &
-    echo "执行完毕"
-          break
- else
-    echo 正在启动各项服务，再次尝试....
-    echo  排队 $sec 秒 , 配置服务第 $var 次尝试
-          sleep $sec
-    echo  "启动失败"
- fi
-done
+#启动授权服务
+nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/user-auth-1.0-SNAPSHOT.jar  >>${base_dir}/user-auth.log 2>&1 &
+sleep $sec
+#启动磅单服务
+nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/business-pound-1.0-SNAPSHOT.jar  >>${base_dir}/business-pound.log 2>&1 &
+sleep $sec
+
+#启动网关服务
+nohup java -Xmx256m -Xms128m -Xss256k -jar $base_dir/gateway-1.0-SNAPSHOT.jar  >>${base_dir}/gateway.log 2>&1 &
+
