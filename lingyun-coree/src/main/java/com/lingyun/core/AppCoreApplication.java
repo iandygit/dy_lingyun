62badc81e2d34333d5aab3f9007d10f4229db115
zhangbo <329989580@qq.com>
2020-11-17 13:45:23 +0800

    增加启动类

---
+++ lingyun-coree/src/main/java/com/lingyun/core/AppCoreApplication.java
@@ -0,0 +1,13 @@
+package com.lingyun.core;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCoreApplication {
+
+    public static void main(String args[]) throws Exception {
+        SpringApplication.run(AppCoreApplication.class);
+    }
+}
